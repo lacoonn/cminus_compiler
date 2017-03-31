b038e0274a86f23f1ab3517d3c7c7e49f7b3ea86
김우찬 <teakan7179@gmail.com>
2017-03-31 12:08:29 +0900

    0330

--- scanner/scanner.c
+++ scanner/scanner.c
@@ -27,18 +27,26 @@ void get_token(FILE *fp_out, char *buf)
 {
 	enum state now_state = START;
 	char temp;
-	char line_buf[SMALLBUF]; //save one line
-	int ptr = 0, line_ptr = 0, word_ptr = 0;
+	char line_buf[SMALLBUF], //save one line
+		words_buf[SMALLBUF]; //save words token
+	int ptr = 0, line_ptr = 0, words_ptr = 0;
 
 
-	while (now_state != FIN)
+	while (now_state != FIN || now_state != ERROR)
 	{
 		temp = buf[ptr];
 		switch (now_state)
 		{
 		case START:
 			if (isWS(buf[ptr]))
+			{
 				now_state = START;
+				if (temp == '\n')
+				{
+					fwrite(line_buf, 1, strlen(line_buf), fp_out);
+					fwrite(words_buf, 1, strlen(words_buf), fp_out);
+				}
+			}
 			else if (isLetter(buf[ptr]))
 				now_state = ID;
 			else if (isDigit(buf[ptr]))
@@ -48,9 +56,14 @@ void get_token(FILE *fp_out, char *buf)
 			else if (temp == '/')
 				now_state = COMMENT1;
 			else
-				now_state = FIN;
+				now_state = ERROR;
 			break;
-			
+		case ID:
+			if (isLetter(temp))
+				now_state = ID;
+
+			break;
+
 
 
 		}
@@ -63,17 +76,26 @@ void get_token(FILE *fp_out, char *buf)
 
 int isLetter(char input)
 {
-
+	if ((input >= 'a' && input <= 'z') || (input >= 'A' && input <= 'Z'))
+		return 1;
+	else
+		return 0;
 }
 
 int isDigit(char input)
 {
-
+	if (input >= '0' && input <= '9')
+		return 1;
+	else
+		return 0;
 }
 
 int isWS(char input)
 {
-
+	if (input == ' ' || input == '\n' || input == '\t')
+		return 1;
+	else
+		return 0;
 }
 
 void error_handling(char *message)
